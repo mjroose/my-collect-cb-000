14d8198cbe21d6d9d171e67e8cd75ab864dd27c1
Michael Roose <mjroose@gmail.com>
2018-08-05 19:47:16 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  if array.length !=0
+    i = 0
+    new_array = []
+    while i < array.length
+      new_array << yield(array[i])
+      i += 1
+    end
+    new_array
+  else
+    array
+  end
+end
